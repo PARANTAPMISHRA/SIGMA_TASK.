1534d72fb7ac8ffa78d087d812d22c10c59887ca
PARANTAPMISHRA <119800240+PARANTAPMISHRA@users.noreply.github.com>
2024-06-08 21:46:46 +0530

    Update webapplication.py

--- webapplication.py
+++ webapplication.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import datetime 
 from PIL import Image
+!pip install plotly 
 import plotly.express as px
 import plotly.graph_objects as go
 from streamlit_option_menu import option_menu
