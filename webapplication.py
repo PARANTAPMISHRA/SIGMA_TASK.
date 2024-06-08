d7040d475119dfb7bdb1ab2a36ff87a9d4aa4dcf
PARANTAPMISHRA <119800240+PARANTAPMISHRA@users.noreply.github.com>
2024-06-09 04:38:17 +0530

    Update webapplication.py

--- webapplication.py
+++ webapplication.py
@@ -14,8 +14,8 @@ with st.sidebar:
 
 
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, index_col=False)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file, index_col=False)
       
       
         dataset_size = df.shape[0]
