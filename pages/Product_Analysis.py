9c89d536d0d43e0e09b92dd6dc325556eb940bba
PARANTAPMISHRA <119800240+PARANTAPMISHRA@users.noreply.github.com>
2024-06-09 12:15:42 +0530

    Update Product_Analysis.py

--- pages/Product_Analysis.py
+++ pages/Product_Analysis.py
@@ -10,7 +10,7 @@ from streamlit_option_menu import option_menu
 st.set_page_config(layout='wide',page_title='EDA',page_icon='bar_chart')
 st.title('Analyzing the most common products')
 with st.sidebar:
-    # Load data
+    
     st.header(' Input data')
 
 
